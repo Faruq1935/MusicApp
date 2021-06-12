08b1256d0c1a4cdc127b6a90a3f874d2b2390871
Faruq1935 <faruqsyarifudin8@gmail.com>
2021-06-12 19:02:40 +0700

    MusikApp

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+// Array list musik 
+let allMusic = [
+  {
+    name: "The Chainsmokers & Coldplay - Something Just Like This",
+    artist: "The Chainsmokers & Coldplay",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Mahou No Honyori",
+    artist: "sui",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "ドリーマーズハイ",
+    artist: "RADWIMPS",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "おもいでしりとり",
+    artist: "DIALOGUE+",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "RADWIMPS - 大丈夫",
+    artist: "RADWIMPS",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-6",
+    src: "music-6"
+  }  
+];
